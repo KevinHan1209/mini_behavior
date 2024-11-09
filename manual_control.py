7dfda6a8a9cb05f2afc1a8061af6f42363f2a662
Hender <hlin01@g.ucla.edu>
2024-11-08 21:44:22 -0600

    Quick fixes

--- manual_control.py
+++ manual_control.py
@@ -6,7 +6,7 @@ from mini_behavior.window import Window
 from mini_behavior.utils.save import get_step, save_demo
 from mini_behavior.grid import GridDimension
 import numpy as np
-import mini_behavior.envs.shaking_a_rattle
+import mini_behavior.envs.picking_up_a_rattle
 
 # Size in pixels of a tile in the full-scale human view
 TILE_PIXELS = 32
@@ -205,7 +205,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
     "--env",
     help="gym environment to load",
-    default='MiniGrid-ShakingARattle-8x8-N2-v0'
+    default='MiniGrid-PickingUpARattle-8x8-N2-v0'
 )
 parser.add_argument(
     "--seed",
--- mini_behavior/envs/__init__.py
+++ mini_behavior/envs/__init__.py
@@ -26,4 +26,4 @@ from mini_behavior.envs.two_room_nav import *
 
 from mini_behavior.envs.preparing_salad_floorplan import *
 from mini_behavior.envs.auto_env import *
-from mini_behavior.envs.shaking_a_rattle import *
+from mini_behavior.envs.picking_up_a_rattle import *
